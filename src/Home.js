2b397c71401578b54b1284b08bed6695fe7c94ed
jananimoorthy <jananipriya314@gmail.com>
2024-03-09 17:29:24 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="home-container">
+        <h3>Welcome to React-App</h3>
+        <img className="home-img" src="https://thumbs.dreamstime.com/b/happy-travel-woman-vacation-concept-funny-traveler-enjoy-her-trip-ready-to-adventure-happy-travel-woman-vacation-concept-118679424.jpg"  />
+      </div>
+    )
+  }
+  
